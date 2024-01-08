ea65d68be46e6911151f7df5e6e8145ed59c8336
Emanuel Rodrigues Galvão <emrogalvao@gmail.com>
2024-01-08 18:58:05 -0300

    Finished jetpack-compose-migration codelab

--- jetpack-compose-migration/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ jetpack-compose-migration/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,13 +16,155 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.annotation.SuppressLint
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
+import com.google.samples.apps.sunflower.R
+import com.google.samples.apps.sunflower.data.Plant
+import com.google.samples.apps.sunflower.theme.SunflowerTheme
+import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
 
 @Composable
-fun PlantDetailDescription() {
-    Surface {
-        Text("Hello Compose")
+fun PlantDetailDescription(
+    plantDetailViewModel: PlantDetailViewModel
+) {
+
+    val plant by plantDetailViewModel.plant.observeAsState()
+
+    plant?.let {
+        PlantDetailContent(plant = it)
+    }
+}
+
+@Composable
+fun PlantDetailContent(plant: Plant) {
+    Column(
+        modifier = Modifier.padding(dimensionResource(id = R.dimen.margin_normal))
+    ) {
+        PlantName(name = plant.name)
+        PlantWatering(wateringInterval = plant.wateringInterval)
+        PlantDescription(description = plant.description)
+    }
+}
+
+@Preview
+@Composable
+fun PlantDetailContentPreview() {
+    val plant = Plant("id", "Apple", "description", 3, 30, "")
+    SunflowerTheme {
+        PlantDetailContent(plant = plant)
+    }
+}
+
+@Composable
+fun PlantName(name: String) {
+    Text(
+        text = name,
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
+}
+
+@Preview
+@Composable
+fun PlantNamePreview() {
+    SunflowerTheme {
+        PlantName(name = "Apple")
+    }
+}
+
+@SuppressLint("ResourceType")
+@Composable
+fun PlantWatering(wateringInterval: Int) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+
+        val centerWithPaddingModifier = Modifier
+            .padding(horizontal = dimensionResource(R.dimen.margin_small))
+            .align(Alignment.CenterHorizontally)
+
+        val normalPadding = dimensionResource(R.dimen.margin_normal)
+
+        Text(
+            text = stringResource(id = R.string.watering_needs_prefix),
+            modifier = centerWithPaddingModifier.padding(top = normalPadding),
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primaryVariant
+        )
+
+        val wateringIntervalText = pluralStringResource(
+            R.plurals.watering_needs_suffix, wateringInterval, wateringInterval
+        )
+        Text(
+            text = wateringIntervalText,
+            modifier = centerWithPaddingModifier.padding(bottom = normalPadding)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PlantWateringPreview() {
+    SunflowerTheme {
+        PlantWatering(wateringInterval = 7)
+    }
+}
+
+@Composable
+fun PlantDescription(
+    description: String
+) {
+
+    val htmlDescription = remember(description) {
+        HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+    }
+
+    AndroidView(
+        factory = { context ->
+            TextView(context).apply {
+                movementMethod = LinkMovementMethod.getInstance()
+            }
+        },
+        update = {
+            it.text = htmlDescription
+        }
+    )
+}
+
+@Preview
+@Composable
+fun PlantDescriptionPreview() {
+    SunflowerTheme {
+        PlantDescription(description = "HTML<br><br>description")
     }
 }
